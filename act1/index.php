203eae664fc8fbc41d823429b8c67cc67ce0197d
Dr3w-Designs <Kapoch19@gmail.com>
2026-04-27 10:54:36 +0800

    2026-04-27 10:52am: creation of act1 and index.php

---
+++ act1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De Jesus - Resume</title>
+</head>
+<body>
+    <?php 
+        $name = "Andrew J. De Jesus";
+        $profession = "IT Specialist - Web and Mobile Applications";
+        $email = "ajdejesus@fit.edu.ph";
+        $phoneNum = "+63-9420600001";
+        $location = "Sta. Maria, Bulacan, Philippines";
+    ?>
+</body>
+    <header>
+        
+    </header>
+    <section>
+
+    </section>
+</html>
