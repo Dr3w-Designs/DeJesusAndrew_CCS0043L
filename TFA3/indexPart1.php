27bdf3c9ec771ad4ee5f49a486d39fad7a4e5412
Logically Dr3w <Kapoch19@gmail.com>
2026-05-18 11:18:04 +0800

    2026-05-18 11:17am: names for indexPart1

---
+++ TFA3/indexPart1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ten People - De Jesus</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <php>
+        $people = [
+            ['name' => 'Cooper, Justin', 'image' => 'image_justin_cooper.jpg', 'age' => 28, 'bDay' => 'Aug 31, 1997', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Hill, Justin', 'image' => 'image_justin_hill.jpg', 'age' => 31, 'bDay' => 'Jan 5, 1995', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Laurence, Hunter', 'image' => 'image_hunter_laurence.jpg', 'age' => 26, 'bDay' => 'Aug 1, 1999', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Prado, Jorge', 'image' => 'image_jorge_prado.jpg', 'age' => 25, 'bDay' => 'Jan 5, 2001', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Roczen, Ken', 'image' => 'image_ken_roczen.jpg', 'age' => 32, 'bDay' => 'Apr 29, 1994', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Savatgy, Joey', 'image' => 'image_joey_savatgy.jpg', 'age' => 32, 'bDay' => 'Feb 21, 1994', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Sexton, Chase', 'image' => 'image_chase_sexton.jpg', 'age' => 26, 'bDay' => 'Sep 23, 1999', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Stewart, Malcolm', 'image' => 'image_malcolm_stewart.jpg', 'age' => 33, 'bDay' => 'Oct 27, 1992', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Tomac, Eli', 'image' => 'image_eli_tomac.jpg', 'age' => 33, 'bDay' => 'Nov 14, 1992', 'cpNum' => '+63 912 345 6789'],
+            ['name' => 'Webb, Cooper', 'image' => 'image_cooper_webb.jpg', 'age' => 30, 'bDay' => 'Nov 10, 1995', 'cpNum' => '+63 912 345 6789'],
+        ];
+</body>
+</html>
