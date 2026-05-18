74c936df28c722ca120e814fb88b731209af97d6
Logically Dr3w <Kapoch19@gmail.com>
2026-05-18 12:08:34 +0800

    2026-05-18 12:08pm: Removed inline css and utilized existing style.css

--- TFA3/indexPart2.php
+++ TFA3/indexPart2.php
@@ -6,30 +6,6 @@
 	<title>Array Operations - De Jesus</title>
 
     <link rel="stylesheet" href="style.css">
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			background-color: #f0f0f0;
-		}
-
-		table {
-			width: 640px;
-			margin: 24px auto;
-			border-collapse: collapse;
-			background-color: #ffffff;
-		}
-
-		th, td {
-			border: 1px solid #333333;
-			padding: 10px 12px;
-			text-align: center;
-		}
-
-		caption {
-			font-weight: bold;
-			padding: 10px 0;
-		}
-	</style>
 </head>
 <body>
 
@@ -50,26 +26,30 @@
 	$list = implode(', ', $numbers);
 ?>
 
-	<table>
-		<caption>Array list: <?php echo $list; ?></caption>
-		<tbody>
-			<tr>
-				<td>Addition</td>
-				<td><?php echo $sum; ?></td>
-			</tr>
-			<tr>
-				<td>Subtraction</td>
-				<td><?php echo $difference; ?></td>
-			</tr>
-			<tr>
-				<td>Multiplication</td>
-				<td><?php echo $product; ?></td>
-			</tr>
-			<tr>
-				<td>Division</td>
-				<td><?php echo $quotient; ?></td>
-			</tr>
-		</tbody>
-	</table>
+	<div class="container">
+		<main class="main">
+			<table class="people-table">
+				<caption>Array list: <?php echo $list; ?></caption>
+				<tbody>
+					<tr>
+						<td>Addition</td>
+						<td><?php echo $sum; ?></td>
+					</tr>
+					<tr>
+						<td>Subtraction</td>
+						<td><?php echo $difference; ?></td>
+					</tr>
+					<tr>
+						<td>Multiplication</td>
+						<td><?php echo $product; ?></td>
+					</tr>
+					<tr>
+						<td>Division</td>
+						<td><?php echo $quotient; ?></td>
+					</tr>
+				</tbody>
+			</table>
+		</main>
+	</div>
 </body>
 </html>
