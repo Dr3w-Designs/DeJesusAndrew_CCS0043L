6b17f8e13b05f700293b8e1ae2ea1c0a9dbfdc2c
Dr3w-Designs <Kapoch19@gmail.com>
2026-06-22 11:11:29 +0800

    2026-06-22 11:11am: created necessary files & started on home page

---
+++ PSA6/homePage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>DJ Dog Information System</h1>
+    
+    <p>Select what you want to do:</p>
+
+    <a href="dogRegistration.php" class="btn">Register Dog</a>
+    <a href="dogView.php" class="btn">View Dogs</a>
+</body>
+</html>
